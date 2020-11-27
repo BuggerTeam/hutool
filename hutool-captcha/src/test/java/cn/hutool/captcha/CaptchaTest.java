25b621b3223e66aefa9bfbfeb46f6046f902f846
蓝田_Loto <shorfng@126.com>
2020-11-27 09:54:55 +0800

    【TD】
    更改验证码图片保存路径为项目中

--- hutool-captcha/src/test/java/cn/hutool/captcha/CaptchaTest.java
+++ hutool-captcha/src/test/java/cn/hutool/captcha/CaptchaTest.java
@@ -16,80 +16,97 @@ import java.awt.*;
  *
  */
 public class CaptchaTest {
+	public static final String PATH = System.getProperty("user.dir")+ "\\src\\test\\java\\cn\\hutool\\captcha\\image\\";
 
+	// 验证码
 	@Test
 	public void lineCaptchaTest1() {
 		// 定义图形验证码的长和宽
 		LineCaptcha lineCaptcha = CaptchaUtil.createLineCaptcha(200, 100);
+
+		// 获取验证码的英文字母
 		Assert.assertNotNull(lineCaptcha.getCode());
+		Console.log(lineCaptcha.getCode());
+
+		// 验证用户输入的字符串是否与生成的验证码匹配
 		Assert.assertTrue(lineCaptcha.verify(lineCaptcha.getCode()));
 	}
 
-	@Test
-	@Ignore
-	public void lineCaptchaTest3() {
-		// 定义图形验证码的长和宽
-		LineCaptcha lineCaptcha = CaptchaUtil.createLineCaptcha(200, 70, 4, 15);
-		lineCaptcha.setBackground(Color.yellow);
-		lineCaptcha.write("f:/test/captcha/tellow.png");
-	}
-	
-	@Test
-	@Ignore
-	public void lineCaptchaWithMathTest() {
-		// 定义图形验证码的长和宽
-		LineCaptcha lineCaptcha = CaptchaUtil.createLineCaptcha(200, 80);
-		lineCaptcha.setGenerator(new MathGenerator());
-		lineCaptcha.setTextAlpha(0.8f);
-		lineCaptcha.write("f:/captcha/math.png");
-	}
-
+	// 验证码 - 内容为数字和字母，背景为线条
 	@Test
 	@Ignore
 	public void lineCaptchaTest2() {
-
 		// 定义图形验证码的长和宽
 		LineCaptcha lineCaptcha = CaptchaUtil.createLineCaptcha(200, 100);
 		// LineCaptcha lineCaptcha = new LineCaptcha(200, 100, 4, 150);
+
 		// 图形验证码写出，可以写出到文件，也可以写出到流
-		lineCaptcha.write("f:/captcha/line.png");
+		lineCaptcha.write(PATH + "image_LineCaptcha1.png");
 		Console.log(lineCaptcha.getCode());
 		// 验证图形验证码的有效性，返回boolean值
 		lineCaptcha.verify("1234");
 
 		lineCaptcha.createCode();
-		lineCaptcha.write("f:/captcha/line2.png");
+		lineCaptcha.write(PATH + "image_LineCaptcha2.png");
 		Console.log(lineCaptcha.getCode());
 		// 验证图形验证码的有效性，返回boolean值
 		lineCaptcha.verify("1234");
 	}
 
+	// 验证码 - 内容为数字和字母，背景为线条
+	@Test
+	@Ignore
+	public void lineCaptchaTest3() {
+		// 定义图形验证码的长和宽
+		LineCaptcha lineCaptcha = CaptchaUtil.createLineCaptcha(200, 70, 4, 15);
+
+		lineCaptcha.setBackground(Color.yellow);
+		lineCaptcha.write(PATH + "image_LineCaptcha3.png");
+	}
+
+	// 验证码 - 内容为数学计算
+	@Test
+	//@Ignore
+	public void lineCaptchaWithMathTest() {
+		// 定义图形验证码的长和宽
+		LineCaptcha lineCaptcha = CaptchaUtil.createLineCaptcha(200, 80);
+
+		lineCaptcha.setGenerator(new MathGenerator());
+		lineCaptcha.setTextAlpha(0.8f);
+		lineCaptcha.write(PATH + "image_LineCaptcha_Math.png");
+	}
+
+	// 验证码 - 内容为字符，背景为圆形
 	@Test
 	@Ignore
 	public void circleCaptchaTest() {
-
 		// 定义图形验证码的长和宽
 		CircleCaptcha captcha = CaptchaUtil.createCircleCaptcha(200, 100, 4, 20);
 		// CircleCaptcha captcha = new CircleCaptcha(200, 100, 4, 20);
+
 		// 图形验证码写出，可以写出到文件，也可以写出到流
-		captcha.write("f:/captcha/circle.png");
+		captcha.write(PATH + "image_CircleCaptcha.png");
+
 		// 验证图形验证码的有效性，返回boolean值
 		captcha.verify("1234");
 	}
 
+	// 验证码 - 内容为数字和字母，背景为扭曲干扰线
 	@Test
 	@Ignore
 	public void ShearCaptchaTest() {
-
 		// 定义图形验证码的长和宽
 		ShearCaptcha captcha = CaptchaUtil.createShearCaptcha(203, 100, 4, 4);
 		// ShearCaptcha captcha = new ShearCaptcha(200, 100, 4, 4);
+
 		// 图形验证码写出，可以写出到文件，也可以写出到流
-		captcha.write("f:/captcha/shear.png");
+		captcha.write(PATH + "image_ShearCaptcha.png");
+
 		// 验证图形验证码的有效性，返回boolean值
 		captcha.verify("1234");
 	}
-	
+
+	// 验证码 - 内容为数学计算，背景为扭曲干扰线
 	@Test
 	@Ignore
 	public void ShearCaptchaWithMathTest() {
@@ -97,8 +114,10 @@ public class CaptchaTest {
 		ShearCaptcha captcha = CaptchaUtil.createShearCaptcha(200, 45, 4, 4);
 		captcha.setGenerator(new MathGenerator());
 		// ShearCaptcha captcha = new ShearCaptcha(200, 100, 4, 4);
+
 		// 图形验证码写出，可以写出到文件，也可以写出到流
-		captcha.write("f:/captcha/shear_math.png");
+		captcha.write(PATH + "image_ShearCaptcha_Math.png");
+
 		// 验证图形验证码的有效性，返回boolean值
 		captcha.verify("1234");
 	}
